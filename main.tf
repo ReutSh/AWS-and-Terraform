c3c0f6ccc5c7450d2b83d0f8f64d84d205775305
reut <shwartza@gmail.com>
2020-10-22 00:38:47 +0300

    I have two unclear errors - can you please direct me??

--- main.tf
+++ main.tf
@@ -1,36 +1,47 @@
  provider "aws" {
-    profile = reut
-    region = us-east-1
+    profile = "reut"
+    region = "us-east-1"
 }
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
+  most_recent = true 
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]  
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-#not sure what this is
-  owners = ["099720109477"] # Canonical
+
+ owners = ["099720109477"]
 }
 
 resource "aws_instance" "Reut_terraform_test" {
   count = 2
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.medium"
+  user_data = file(nginx.sh)
+  key_name = "Reut"
   subnet_id = "subnet-01792509a3e220b16"
-
+  }
+  
+  
   ebs_block_device  {
   device_name = "xvdf"
-  volume_size = 10 
-} 
-  
+  instance_id = aws_ami.ubuntu.id
+  volume_id = gp2
+  volume_size = 10
+    }
+
+
   tags = {
-  Name = "Reut_server ${count.index}"
-  purpose = "Terraform hw exe4"
+  Name = "Reut_terraform_test ${count.index}"
+  purpose = "Terraform hw exe4" 
+  }
 }
+
+
+
