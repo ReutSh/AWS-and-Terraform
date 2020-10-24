e182c50eb182621607cf10bc29e8f3cf84914acb
reut <shwartza@gmail.com>
2020-10-24 16:48:28 +0300

    with AZ definition

--- nginx.sh
+++
@@ -1,5 +0,0 @@
-apt update
-apt install -y nginx
-sed -i 's/Welcome to nginx!/Ops-School Rules!/g' /var/www/index.nginx-debian.html 
-sed -i '15,23d;' /var/www/html/index.nginx-debian.html 
-systemctl restart nginx
